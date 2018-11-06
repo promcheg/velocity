ec4ed416b839291c6e009d0e7ee7f8dce4a63ad2
wamu <waldemar.mueller-ext@idemtelematics.com>
2018-11-06 22:21:11 +0100

    formating

--- src/promcheg/api/utils/GraphUtils.java
+++ src/promcheg/api/utils/GraphUtils.java
@@ -1,31 +1,62 @@
 package promcheg.api.utils;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 import org.eclipse.swt.graphics.Point;
 
+import promcheg.api.Constants;
+
 public class GraphUtils {
+	/**
+	 * 
+	 * @param rx
+	 * @param ry
+	 * @param w
+	 * @param h
+	 * @return
+	 */
 	public static  Point toDisplayCoordinates(BigDecimal rx, BigDecimal ry, int w, int h) {
-		BigDecimal px = rx.add(BigDecimal.valueOf(1)).multiply(BigDecimal.valueOf(w)).divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP);
+		BigDecimal px = rx
+										.add(BigDecimal.valueOf(1))
+										.multiply(BigDecimal.valueOf(w))
+										.divide(BigDecimal.valueOf(2), 
+												Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE);
 		
-		BigDecimal py = BigDecimal.valueOf(h).subtract(BigDecimal.valueOf(h).multiply(ry)).divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP);	
+		BigDecimal py = BigDecimal.valueOf(h)
+										.subtract(BigDecimal.valueOf(h)
+										.multiply(ry))
+										.divide(BigDecimal.valueOf(2),
+												Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE);	
 		
 		return new Point(px.intValue(), py.intValue());
 	}
 	
+	/**
+	 * 
+	 * @param px
+	 * @param py
+	 * @param w
+	 * @param h
+	 * @return
+	 */
 	public static  BigDecimal[] toRelativeCoordinates(int px, int py, int w, int h) {
 		BigDecimal rx = BigDecimal.valueOf(px)
 													.subtract(BigDecimal.valueOf(w)
-													.divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP))
+													.divide(BigDecimal.valueOf(2),
+															Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE))
 													.divide(BigDecimal.valueOf(w)
-													.divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP), 10, RoundingMode.HALF_UP);
+															.divide(BigDecimal.valueOf(2),
+																	Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE), 
+															Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE);
 		BigDecimal ry = BigDecimal.valueOf(-1)
 													.multiply(BigDecimal.valueOf(py)
 													.subtract(BigDecimal.valueOf(h)
-													.divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP))
+													.divide(BigDecimal.valueOf(2),
+															Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE))
 													.divide(BigDecimal.valueOf(h)
-													.divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP), 10, RoundingMode.HALF_UP));
+															.divide(BigDecimal.valueOf(2),
+																	Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE), 
+															Constants.MATH_PRECISION, Constants.MATH_ROUNDING_MODE));
 		
 		return new BigDecimal[] {rx, ry};
 	}
--- src/promcheg/velocity/VelocityDisplay.java
+++ src/promcheg/velocity/VelocityDisplay.java
@@ -22,11 +22,9 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 public class VelocityDisplay  {
 	private int width;
